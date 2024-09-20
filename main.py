5f981e6e311f8df394a46fe3ad7688adc4747c59
Juan Martin Zabala Ochoa <63268981+juanmartin8a@users.noreply.github.com>
2024-09-20 09:20:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ def stocksEmail():
 
 
 
-dataOrEmail = input('get stocks data or send updates via email 1/2: ')
+dataOrEmail = input('add stocks data or send updates via email? Press 1 too ad data or 2 to send updates via email: ')
 if dataOrEmail == '1':
     market = input('Type in a market (aapl, etc): ')
     page_url = 'https://markets.businessinsider.com/stocks/' + market + '-stock'
